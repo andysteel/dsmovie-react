4dde68fefe1694bcd1c9462afff1802afa16cf04
Anderson Dias <andersoninfonet@gmail.com>
2022-01-11 00:19:29 -0300

    Routes

---
+++ src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+const Form = () => {
+
+  return (
+    <h1>Pagina de formulario</h1>
+  )
+}
+
+export default Form
---
+++ src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+const Listing = () => {
+
+  return (
+    <h1>Pagina de listagem</h1>
+  )
+}
+
+export default Listing
