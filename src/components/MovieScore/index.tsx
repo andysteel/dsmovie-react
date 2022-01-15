a9695c6cbf2a13629bf45374cdbdc34a981b6500
Anderson Dias <andersoninfonet@gmail.com>
2022-01-14 23:59:25 -0300

    show score

--- src/components/MovieScore/index.tsx
+++ src/components/MovieScore/index.tsx
@@ -1,15 +1,17 @@
 import MovieStars from "components/MovieStars"
 import "./styles.css"
 
-const MovieScore = () => {
+interface MovieScoreProps {
+  score: number;
+  count: number;
+}
 
-  const score = 3.8
-  const count = 13
+const MovieScore = ({score, count}: MovieScoreProps) => {
 
   return (
     <div className="dsmovie-score-container">
       <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-      <MovieStars />
+      <MovieStars score={score} />
       <p className="dsmovie-score-count">{count} avaliações</p>
     </div>
   )
