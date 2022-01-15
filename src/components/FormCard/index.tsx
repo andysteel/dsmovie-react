c07b81161c044dfca021a3e6b075468e18b57299
Anderson Dias <andersoninfonet@gmail.com>
2022-01-15 00:53:19 -0300

    Save, useNavigate

--- src/components/FormCard/index.tsx
+++ src/components/FormCard/index.tsx
@@ -1,8 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import axios, { AxiosRequestConfig } from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
 import { BASE_URL } from "utils/request";
+import { validateEmail } from "utils/validate";
 import "./styles.css"
 
 interface FormCardProps {
@@ -11,22 +12,51 @@ interface FormCardProps {
 
 const FormCard = ({ movieId }: FormCardProps) => {
 
+  const navigate = useNavigate()
   const [movie, setMovie] = useState<Movie>()
+  const [validEmail, setValidEmail] = useState(true)
 
   useEffect(() => {
     axios.get(`${BASE_URL}/movies/${movieId}`)
       .then(response => setMovie(response.data))
   }, [movieId])
 
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const email = (event.target as any).email.value
+    const score = (event.target as any).score.value
+
+    if(!validateEmail(email)) {
+      setValidEmail(false)
+      return;
+    }
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/scores',
+      data: {
+        email,
+        score,
+        movieId
+      }
+    }
+
+    axios(config)
+    .then(response => navigate('/'))
+    .catch(error => console.log(error))
+  }
+
   return (
     <div className="dsmovie-form-container">
         <img className="dsmovie-movie-card-image" src={movie?.image} alt="The Witcher" />
         <div className="dsmovie-card-bottom-container">
             <h3>{movie?.title}</h3>
-            <form className="dsmovie-form">
+            <form className="dsmovie-form" onSubmit={handleSubmit}>
                 <div className="form-group dsmovie-form-group">
+                    {!validEmail && <p className="email-message-error">Email inválido.</p>}
                     <label htmlFor="email">Informe seu email</label>
-                    <input type="email" className="form-control" id="email" />
+                    <input type="text" className={`form-control ${validEmail ? '' : 'email-error'}`} id="email" />
                 </div>
                 <div className="form-group dsmovie-form-group">
                     <label htmlFor="score">Informe sua avaliação</label>
